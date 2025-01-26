1a918f356a4abee4c00dcf898b36bb5d6150d4c8
faizal <faisalt661@gmail.com>
2025-01-26 14:40:54 +0530

    Connected to MongoDB atlas.created userSchema.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,13 +1,15 @@
 
 
 export const signup=(req,res)=>{
-    
+    res.send('signup page is loaded')
 }
 
 export const login=(req,res)=>{
+    res.send('login page is loaded')
     
 }
 
 export const logout=(req,res)=>{
+    res.send('logout successfully ')
     
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,19 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
 import authRoutes from './routes/auth.route.js'
+import { connectDB } from './lib/db.js'
 
 
-const app= express()
+const app = express()
 
-app.use('/api/auth',authRoutes)
+const PORT = process.env.PORT || 3333
 
-app.listen(3333,()=>{
-    console.log('Server is started on port 3333')
-}) 
+app.use('/api/auth', authRoutes)
+
+app.listen(PORT, () => {
+    console.log('Server is started on port :',PORT)
+    connectDB()
+})
+
